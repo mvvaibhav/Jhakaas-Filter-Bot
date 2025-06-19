3cb90733f5db11797ff31fd892c3d15a33cdf99d
mv vaibhav <154995597+mvvaibhav@users.noreply.github.com>
2025-06-20 04:24:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,8 +55,8 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+cndEFVJSm-cxYTc1')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/I_M_STARBOY')
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/+cndEFVJSm-cxYTc1')
 
-AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-100'))
-AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-100'))
+AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002817882092'))
+AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002817882092'))
 
 IS_VERIFY = is_enabled('IS_VERIFY', False)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '100'))
